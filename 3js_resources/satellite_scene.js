2a51216dc49c0eeb17b8e5782240cc758c30cc09
Louis <louismc4@hotmail.com>
2017-11-10 07:29:41 +0000

    update 2

--- 3js_resources/satellite_scene.js
+++ 3js_resources/satellite_scene.js
@@ -7,14 +7,15 @@ earthRadius = 455, sat_coords = [0,0,0],
 
 windowHalfX = window.innerWidth / 2,
 windowHalfY = window.innerHeight / 2,
+stats, xPanel, yPanel,
 
 SEPARATION = 200,
 AMOUNTX = 10,
 AMOUNTY = 10,
 
-camera, cameraController, scene, renderer;
+camera, cameraController, scene, renderer, controls;
 
-var cube_sat;
+var cube_sat, light, earthMesh;
 
 init();
 animate();
@@ -27,7 +28,7 @@ function init() {
     container = document.createElement('div');
     document.body.appendChild(container);
 
-    camera = new THREE.PerspectiveCamera( 75, SCREEN_WIDTH / SCREEN_HEIGHT, 1, 10000 );
+    camera = new THREE.PerspectiveCamera( 75, SCREEN_WIDTH / SCREEN_HEIGHT, 0.1, 10000 );
     camera.position.z = 1000;
     cameraController = new THREE.Object3D();
     cameraController.add(camera);
@@ -35,39 +36,127 @@ function init() {
     scene = new THREE.Scene();
     scene.add(cameraController);
 
-    renderer = new THREE.CanvasRenderer();
+    renderer = new THREE.WebGLRenderer({antialiasing : true});
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
     container.appendChild( renderer.domElement );
+    
+    // info display
+    stats = new Stats();
+    xPanel = stats.addPanel( new Stats.Panel( ': long', '#ff8', '#221' ) );
+    yPanel = stats.addPanel( new Stats.Panel( ': lat', '#5f8', '#221' ) );
+    zPanel = stats.addPanel( new Stats.Panel( ': alt', '#46f', '#221' ) );
+    stats.showPanel( 3 );
+    container.appendChild( stats.dom );
+    
+    //controls
+    controls = new THREE.OrbitControls( camera, renderer.domElement);
+    controls.addEventListener( 'change', render );
 
-    var geometry = new THREE.SphereGeometry( 455, 32, 32 );
-    var material = new THREE.MeshPhongMaterial()
-    material.map = THREE.ImageUtils.loadTexture('3js_resources/textures/Albedo.jpg')
+    //var earthMesh, Atmos, AtmosMat;
+    var Atmos, AtmosMat;
+    light = new THREE.SpotLight(0xFFFFFF, 1, 0, Math.PI / 2, 1);
+    light.position.set(4000, 4000, 1500);
+    light.target.position.set (1000, 3800, 1000);
+    light.castShadow = true;
+    scene.add(light);
+
+    //EARTH
+    var earthGeo = new THREE.SphereGeometry (455, 400, 400),
+        earthMat = new THREE.MeshPhongMaterial();
+    earthMesh = new THREE.Mesh(earthGeo, earthMat);
+                
+    earthMesh.position.set(0, 0, 0);
+    scene.add(earthMesh);
+    var temp_coords = latLongToCoords(sat_latitudeStr, sat_longitudeStr, earthRadius + 60);
+    var sat_pos = new THREE.Vector3(temp_coords[0], temp_coords[1], temp_coords[2]);
+    if (distanceBetween(camera.position, sat_pos) > distanceBetween(camera.position, earthMesh.position)) {
+        camera.position.z = -1000;
+    }
+                
+    //diffuse
+    earthMat.map = THREE.ImageUtils.loadTexture('./3js_resources/textures/Albedo_medium.jpg');
+    //bump
+    earthMat.bumpMap = THREE.ImageUtils.loadTexture('./3js_resources/textures/bump-map.jpg');
+    earthMat.bumpScale = 8;
+    //specular
+    earthMat.specularMap = THREE.ImageUtils.loadTexture('./3js_resources/textures/earthspec1k.jpg');
+    earthMat.specular = new THREE.Color('#2e2e2e');
+               
+    earthMesh.castShadow = true;
+    earthMesh.receiveShadow = true;
+       
+    //Atmosphere
+    AtmosMat = new THREE.ShaderMaterial({
+      uniforms:{
+        "c": { type: "f", value: 0.3 },
+        "p": { type: "f", value: 5.2},
+        glowColor: { type: "c", value: new THREE.Color(0x00dbdb)},
+        viewVector: { type: "v3", value: camera.position}
+      },
+      vertexShader: document.getElementById('vertexShader').textContent,
+      fragmentShader: document.getElementById('fragmentShader').textContent,
+      side: THREE.BackSide,
+      blending: THREE.AdditiveBlending,
+      transparent: true
+    });
+
+    Atmos = new THREE.Mesh(earthGeo, AtmosMat);
+    Atmos.position = earthMesh.position;
+    Atmos.position.x -= 50;
+//    Atmos.position.x += 10;
+//    Atmos.position.y += 10;
+    Atmos.scale.multiplyScalar(1.25);
+    scene.add(Atmos);
+
+    //STARS
+    var starGeo = new THREE.SphereGeometry (3000, 10, 100),
+        starMat = new THREE.MeshBasicMaterial();
+    starMat.map = THREE.ImageUtils.loadTexture('./3js_resources/textures/star-field.png');
+    starMat.side = THREE.BackSide;
+                
+    var starMesh = new THREE.Mesh(starGeo, starMat);
+                
+    scene.add(starMesh);
+    
+    // OLD
+    
+    //var geometry = new THREE.SphereGeometry( 455, 256, 256 );
+    //var material = new THREE.MeshPhongMaterial()
+	//var material = new THREE.MeshBasicMaterial();
+    //material.map = THREE.ImageUtils.loadTexture('3js_resources/textures/Albedo.jpg')
     // var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
+    //var cube = new THREE.Mesh( geometry, material );
 //    cube.rotation.y += 0.4;
 //    cube.rotation.z += 0.15;
 //    cube.rotation.x += 0;
-    scene.add( cube );
+    //scene.add( cube );
+	
+	//var light = new THREE.AmbientLight( 0x404040 ); // soft white light
+	//scene.add( light );
     
     var loader = new THREE.JSONLoader();
-    loader.load('3js_resources/model/ExAlta1_low.json', function(geometry) {
-        //cube_sat = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
+    loader.load('3js_resources/model/ExAlta1_low.json', function(geometry, materials) {
+        cube_sat = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
         //cube_sat = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial(materials));
-		cube_sat = new THREE.Mesh(geometry);
-        console.log("Meow", ": ", cube_sat);
+		//cube_sat = new THREE.Mesh(geometry);
+        
+		cube_sat.scale.x = cube_sat.scale.y = cube_sat.scale.z = 40;		
+		scene.add( cube_sat );
+		//scene.children[2].scale = new THREE.Vector3(10,10,10);
+		/*
         if (cube_sat != null) {
+			cube_sat.scale.x = cube_sat.scale.y = cube_sat.scale.z = 5;
             scene.add( cube_sat );
-        }
+			console.log("MEOW2", ": ", scene);
+        } else {
+			var geometry = new THREE.SphereGeometry( 5, 32, 32 );
+			var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+			cube_sat = new THREE.Mesh( geometry, material );
+			scene.add( cube_sat );
+		}*/
         
     });
-    
-    if (cube_sat == null) {
-        var geometry = new THREE.SphereGeometry( 5, 32, 32 );
-        var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-        cube_sat = new THREE.Mesh( geometry, material );
-        scene.add( cube_sat );
-    }
     
 
     // change to a key event listener or look up a better way to do this
@@ -106,6 +195,13 @@ function onGetKeyDown ( event ) {
         cameraController.scale.y -= 0.04;
         cameraController.scale.z -= 0.04;
     }
+}
+
+function distanceBetween(point1, point2) {
+    var dx = point2.x - point1.x;
+    var dy = point2.y - point1.y;
+    var dz = point2.z - point1.z;
+    return Math.sqrt((dx*dx) + (dy*dy) + (dz*dz));
 }
 
 function latLongToCoords(latitude, longitude, radius) {
@@ -172,13 +268,15 @@ function onDocumentTouchMove( event ) {
 
 function updateSatellite() {
     sat_coords = latLongToCoords(sat_latitudeStr, sat_longitudeStr, earthRadius + 60);
-    
-    cube_sat.position.x = sat_coords[0];
-    cube_sat.position.y = sat_coords[1];
-    cube_sat.position.z = sat_coords[2];
-    cameraController.position.x = cube_sat.position.x;
-    cameraController.position.y = cube_sat.position.y;
-    cameraController.position.z = cube_sat.position.z;
+    if (cube_sat != null) {
+		cube_sat.position.x = sat_coords[0];
+		cube_sat.position.y = sat_coords[1];
+		cube_sat.position.z = sat_coords[2];
+        
+		//cameraController.position.x = cube_sat.position.x;
+		//cameraController.position.y = cube_sat.position.y;
+		//cameraController.position.z = cube_sat.position.z;
+	}
     //console.log(sat_coords);
 }
 
@@ -187,8 +285,18 @@ function updateSatellite() {
 function animate() {
 
     requestAnimationFrame( animate );
-
+    controls.update();
     render();
+    //stats.update();
+    //var x = 296;
+    stats.begin();
+    var x = sat_longitudeStr;
+    var y = sat_latitudeStr;
+    var z = sat_altitude;
+    stats.end();
+    xPanel.update( x, 180 );
+    yPanel.update( y, 90 );
+    zPanel.update( z, 1000 );
 
 }
 
@@ -203,8 +311,10 @@ function render() {
     //camera.position.x = sat_coords[0]+200;
     //camera.position.y = sat_coords[1]+200;
     //camera.position.z = sat_coords[2]+200;
+    light.position.set(camera.position.x, camera.position.y, camera.position.z);
     camera.lookAt( cube_sat.position );
 
-    renderer.render( scene, camera );
+    renderer.clear();
+    renderer.render(scene, camera); 
 
 }
---
+++ README.md
@@ -0,0 +1,2 @@
+# ASatVisual
+AlbertaSat Visualization of satellite trajectory for their website
